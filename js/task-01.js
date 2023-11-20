43ebc92cda9ac45a3bec94915c6f4baa4612d7bd
Maria <smalmaria833@gmail.com>
2023-11-20 19:50:27 +0200

    goit-js-hw-06

---
+++ js/task-01.js
@@ -0,0 +1,17 @@
+// Number of categories: 3
+const categories = document.querySelectorAll(".item");
+console.log("Number of categories: " + categories.length);
+
+// Category: Animals
+// Elements: 4
+
+// Category: Products
+// Elements: 3
+
+// Category: Technologies
+// Elements: 5
+const titlesArray = document.querySelectorAll("h2");
+titlesArray.forEach((title) => {
+  console.log("Category:", title.textContent);
+  console.log("Elements:", title.nextElementSibling.children.length);
+});
